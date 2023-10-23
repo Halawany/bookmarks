b266e2db7c0f69869af2344ddfbe8742e0b78f4e
Eslam Halawany <halawany.me@gmail.com>
2023-10-23 14:37:56 +0300

    Create Bookmark model

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bookmark(models.Model):
+    title = models.CharField(max_length=600)
+    url = models.URLField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
