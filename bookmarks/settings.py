36b72ce5f41488d7f7189e5e884e60689e239bdb
Eslam Halawany <halawany.me@gmail.com>
2023-10-24 15:01:30 +0300

    Created github folder

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'bookmarks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Cairo'
 
 USE_I18N = True
 
