7e61e86bd5f7294ed57e629f9124d65a7e343aa1
Eslam Halawany <halawany.me@gmail.com>
2023-10-27 12:36:15 +0200

    Fixed typos in settings and urls

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+// Navbar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
