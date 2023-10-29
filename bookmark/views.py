fe2a8c6e030102528ca914d6f375b799eba6b33a
Eslam Halawany <halawany.me@gmail.com>
2023-10-29 15:31:55 +0200

    Added ListView, now the bookmarks page display the bookmarks

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from .models import Bookmark
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     
+    model = Bookmark
     template_name = "bookmark/home.html"
+    context_object_name = 'bookmarks'
