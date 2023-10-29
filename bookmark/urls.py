046eba4b5ad32502111f4910beb3c9f4c9cd3636
Eslam Halawany <halawany.me@gmail.com>
2023-10-29 15:23:57 +0200

    Added some new styles for bookmarks page

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,69 @@
 // Navbar
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-  }
-  
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Bookmarks box
+// document.querySelector('.box').addEventListener('click', function () {
+//   // Example: Alert a message when the box is clicked
+//   alert('Box clicked! You can perform actions here.');
+// });
+
+// Edit popup
+const editButton = document.querySelector('.option.edit');
+const editPopup = document.querySelector('.popup#editPopup');
+const editClose = document.querySelector('.close#editClose');
+const saveEditButton = document.getElementById('saveEdit');
+const cancelEditButton = document.getElementById('cancelEdit');
+
+// Initially hide the popup
+editPopup.style.display = 'none';
+
+editButton.addEventListener('click', () => {
+  editPopup.style.display = 'block';
+});
+
+editClose.addEventListener('click', () => {
+  editPopup.style.display = 'none';
+});
+
+saveEditButton.addEventListener('click', () => {
+  // Handle saving the edit data here
+  editPopup.style.display = 'none';
+});
+
+cancelEditButton.addEventListener('click', () => {
+  // Handle canceling the edit action here
+  editPopup.style.display = 'none';
+});
+
+// Delete popup
+const deleteButton = document.querySelector('.option.delete');
+const deletePopup = document.querySelector('.popup#deletePopup');
+const deleteClose = document.querySelector('.close#deleteClose');
+const confirmDeleteButton = document.getElementById('confirmDelete');
+const cancelDeleteButton = document.getElementById('cancelDelete');
+
+// Initially hide the delete popup
+deletePopup.style.display = 'none';
+
+deleteButton.addEventListener('click', () => {
+  deletePopup.style.display = 'block';
+});
+
+deleteClose.addEventListener('click', () => {
+  deletePopup.style.display = 'none';
+});
+
+confirmDeleteButton.addEventListener('click', () => {
+  // Handle the delete action here
+  deletePopup.style.display = 'none';
+});
+
+cancelDeleteButton.addEventListener('click', () => {
+  deletePopup.style.display = 'none';
+});
