bad80275922a9e2c90ddcf2f56b6063d5c8fe80b
Eslam Halawany <halawany.me@gmail.com>
2023-11-13 14:32:47 +0200

    New feature Add bookmark

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import HomeView, BookmarkUpdateView, DeleteBookmark
+from .views import HomeView, BookmarkUpdateView, DeleteBookmark, NewBookmark
 
 urlpatterns = [
     path('', HomeView.as_view(), name='bookmarks'),
     path('edit-bookmark/<int:pk>', BookmarkUpdateView.as_view(), name='edit_bookmark'),
     path('delete/<int:pk>', DeleteBookmark.as_view(), name='delete_bookmark'),
+    path('new-bookmark', NewBookmark.as_view(), name='new_bookmark'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Bookmark
@@ -9,6 +9,17 @@ class HomeView(ListView):
     template_name = "bookmark/home.html"
     context_object_name = 'bookmarks'
 
+class NewBookmark(CreateView):
+    
+    model = Bookmark
+    template_name = "bookmark/new.html"
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmarks')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 class BookmarkUpdateView(UpdateView):
 
     model = Bookmark
