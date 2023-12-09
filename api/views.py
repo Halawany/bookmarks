6a051dd8e3d59dc4c9ce618fdc559900530b8a84
Eslam Halawany <halawany.me@gmail.com>
2023-12-09 16:17:40 +0200

    Fixed Bug: user can send a post request with author==another user

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import BookmarkSerializer, BookmarkUpdateViewSerializer
 
 from bookmark.models import Bookmark
@@ -9,17 +9,20 @@ class BookmarkListCreateAPIView(generics.ListCreateAPIView):
     
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
         return Bookmark.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user)
 
 class BookmarkUpdateAPIView(generics.RetrieveUpdateAPIView):
     
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkUpdateViewSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
@@ -29,7 +32,7 @@ class BookmarkDeleteAPIView(generics.RetrieveDestroyAPIView):
     
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
